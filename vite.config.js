e40f394fa7dffa23a29ddcd836599a5e39f4ee9b
AntonNageh <92064286+AntonNageh@users.noreply.github.com>
2023-11-04 19:19:52 +0200

    AddedPWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
+import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(),
+		VitePWA({  
+			registerType: 'prompt',  
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png'],  
+			manifest: {  
+		name: 'Portfolio',  
+			  short_name: 'Portfolio',  
+			  description: 'Anton Nageh Portfolio',  
+			  theme_color: '#ffffff',  
+			  start_url: '/',  
+			  icons: [  
+		{  
+		src: 'pwa-192x192.png',  
+				  sizes: '192x192',  
+				  type: 'image/png',  
+				},  
+				{  
+		src: 'pwa-512x512.png',  
+				  sizes: '512x512',  
+				  type: 'image/png',  
+				},  
+			  ],  
+			},  
+		  }),  
+		]
 });
